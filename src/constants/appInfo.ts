b8699ba7b36cd98b173c34f43e61e6bee0987b14
Reynaldo GT <larz.dev@gmail.com>
2024-01-31 14:48:46 -0400

    changed some metatags

--- src/constants/appInfo.ts
+++ src/constants/appInfo.ts
@@ -1,11 +1,11 @@
 export const logoApp =
   'https://res.cloudinary.com/daij4l3is/image/upload/v1691522537/kdrragm16ba8srie54l0.webp'
 
-export const title = 'BraillEasy'
+export const title = 'AprendEnseñas'
 export const url = 'https://braillease-git-main-larzlrael.vercel.app/'
 
 export const description =
-  'BraillEasy es una aplicación que permite a las personas con discapacidad visual aprender braille de una manera sencilla y divertida.'
+  'AprendEnseñas es una aplicación que te permite escribir en señas y aprender a leerlo.'
 
 export function descriptionMeta(phrase: string) {
   return {
